c791f47bfd93301aaf8c4f921221bfcddf19edf2
Maurice Roy <mautayro@gmail.com>
2017-11-30 20:40:11 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -45,7 +45,7 @@ def get_average_age_for_season(data, season)
   number_of_contestants = 0
   data[season].each do |person, person_data|
     number_of_contestants += 1
-    age_total += person["age"]
+    age_total += person["age"].to_i
   end
   (age_total/number_of_contestants)
 end
