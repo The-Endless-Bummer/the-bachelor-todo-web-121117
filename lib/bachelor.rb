2f9b80093f1873cd31df5498e2017a881638d215
Maurice Roy <mautayro@gmail.com>
2017-11-30 20:29:12 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  count = 0
+  data.each do |season, season_data|
+    season_data.each do |person, person_data|
+      if person["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
